3384aad500272b2487cd8d2ca9c0b9cd7b0281ee
Vincent <mulletvincent0@gmail.com>
2026-06-06 16:51:12 +0800

    Fix: Remove process.exit() and add connection timeouts

--- mullet-server/config/db.js
+++ mullet-server/config/db.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 30000,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    throw error; // Don't call process.exit()!
   }
 };
 
