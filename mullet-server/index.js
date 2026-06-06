b3e87254706aed3545fae89537cdc75dca57870f
Vincent <mulletvincent0@gmail.com>
2026-06-06 16:09:09 +0800

    Deployment

--- mullet-server/index.js
+++ mullet-server/index.js
@@ -43,5 +43,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
